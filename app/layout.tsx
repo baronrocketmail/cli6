3fd1769e990c4249f93a3de0f537dd6aabff78d3
baronrocketmail <baronrocketmail@yahoo.com>
2022-11-02 14:46:00 -0700

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,37 @@
 import "./globals.css"
-import localFont from '@next/font/local';
-/* eslint-disable @next/next/no-head-element */
-//import { Inter } from '@next/font/google';
 
-// Font files can be colocated inside of `app`
-const myFont = localFont({ src: '../public/fonts/GoldmanSans_W_Rg.woff2' });
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+    weight: '300',
+    // You can also pass an array for multiple font weights, e.g. ['400', '600'].
+});
 
 export default function RootLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
-            <html lang="en" className={myFont.className}>
+            <html lang="en" className={roboto.className}>
                 <body>{children}</body>
             </html>
             );
 }
+//import localFont from '@next/font/local';
+///* eslint-disable @next/next/no-head-element */
+////import { Inter } from '@next/font/google';
+//
+//// Font files can be colocated inside of `app`
+//const myFont = localFont({ src: '../public/fonts/GoldmanSans_W_Rg.woff2' });
+//
+//export default function RootLayout({ children }: {
+//    children: React.ReactNode;
+//}) {
+//    return (
+//            <html lang="en" className={myFont.className}>
+//                <body>{children}</body>
+//            </html>
+//            );
+//}
 
 
 //// If loading a variable font, you don't need to specify the font weight
